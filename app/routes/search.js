f567d824eb5ec5c64bd2b7c3c9484444a085c867
Max Larson <mr.m.larson90>
2017-04-25 16:23:42 -0700

    add a website

--- app/routes/search.js
+++ app/routes/search.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(){
+    return this.store.findAll('topStory');
+  },
+  actions: {
+    homie(){
+      this.transitionTo('index');
+    },
+  }
 });
